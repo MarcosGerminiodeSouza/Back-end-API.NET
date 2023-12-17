41513c40d56e252a21a425dc34b06932dfab3ef5
MarcosGerminiodeSouza <marcos.germiniodesouza@gmail.com>
2023-12-17 20:25:51 -0300

    LivrariaAPI Controller CRUD Http

--- Controllers/LivrariaController.cs
+++ Controllers/LivrariaController.cs
@@ -9,58 +9,79 @@ using Microsoft.EntityFrameworkCore;
 
 namespace LivrariaAPI.Controllers
 {
-    [Route("api/[controller]")]
     [ApiController]
+    [Route("api/[controller]")]
     public class LivrariaController : ControllerBase
     {
-        private readonly LivrariaDbContext _contexto;
+        private readonly LivrariaDbContext _context;
 
-        public LivrariaController(LivrariaDbContext contexto)
+        //Injeção de dependencias.
+        public LivrariaController(LivrariaDbContext context)
         {
-            _contexto = contexto;
-
-            foreach (Livro x in _contexto.Livros)
-                _contexto.Livros.Remove(x);
-            _contexto.SaveChanges();
-
-            _contexto.Livros.Add(new Livro { Id = 1, Nome = "CHERLOCK HOMES", Autor = "Arthur Conan Doyle", Categoria = "Mistério", Preco = 80, Quantidade = 5, Imagem = "img1" });
-            _contexto.Livros.Add(new Livro { Id = 2, Nome = "O MUNDO DE SOFIA", Autor = "Jostein Gaarder", Categoria = "Românce", Preco = 20, Quantidade = 6, Imagem = "img2" });
-            _contexto.Livros.Add(new Livro { Id = 3, Nome = "ARCÈNE LUPIN O Ladrão de Casaca", Autor = "Maurice Leblanc", Categoria = "Ficção", Preco = 30, Quantidade = 10, Imagem = "img3" });
-            _contexto.Livros.Add(new Livro { Id = 4, Nome = "INTRODUÇÃO À PROGRAMAÇÃO", Autor = "Anita Lopes", Categoria = "Programação", Preco = 120, Quantidade = 9, Imagem = "img4" });
-            _contexto.Livros.Add(new Livro { Id = 5, Nome = "Guia Front-End", Autor = "Diego ES", Categoria = "Programação", Preco = 110, Quantidade = 8, Imagem = "img5" });
-            _contexto.Livros.Add(new Livro { Id = 6, Nome = "Aprenda a Programar com C#", Autor = "António Trigo", Categoria = "Programação", Preco = 130, Quantidade = 10, Imagem = "img6" });
-            _contexto.Livros.Add(new Livro { Id = 7, Nome = "Use a Cabeça! C#", Autor = "Andrew Stellman", Categoria = "Programação", Preco = 100, Quantidade = 5, Imagem = "img7" });
-            _contexto.Livros.Add(new Livro { Id = 8, Nome = "Introdução à linguagem SQL", Autor = "Tomas Nield", Categoria = "Banco de Dados", Preco = 180, Quantidade = 10, Imagem = "img8" });
-            _contexto.Livros.Add(new Livro { Id = 9, Nome = "MySQL Aprendendo na Prática", Autor = "Sérgio Luiz Tonsig", Categoria = "Banco de Dados", Preco = 150, Quantidade = 7, Imagem = "img9" });
-            _contexto.Livros.Add(new Livro { Id = 10, Nome = "O PODER DO HÁBITO", Autor = "Charles Duhigg", Categoria = "Autoajuda", Preco = 50, Quantidade = 6, Imagem = "img10" });
-
-            _contexto.SaveChanges();
+            _context = context;
         }
 
-        [HttpPost]
-        public async Task<ActionResult<Livro>> PostLivro(Livro livro)
+        [HttpPost("Novo")]
+        public async Task<ActionResult<Livro>> CriarLivro(Livro livro)
         {
-            _contexto.Livros.Add(livro);
-            await _contexto.SaveChangesAsync();
+            _context.Add(livro);
+            await _context.SaveChangesAsync();
 
-            return CreatedAtAction(nameof(ObterPorId), new { id = livro.Id }, livro);
+            return CreatedAtAction(nameof(ObterLivro), new {id = livro.Id}, livro);
         }
 
-        [HttpGet]
-        public async Task<ActionResult<IEnumerable<Livro>>> ObterTudo()
+        [HttpGet("Listar/{id}")]
+        public async Task<ActionResult<Livro>> ObterLivro(int id)
         {
-            return await _contexto.Livros.ToListAsync();
-        }
+            var livroBanco = await _context.Livros.FindAsync(id);
 
-        [HttpGet("{id}")]
-        public async Task<ActionResult<Livro>> ObterPorId(int id)
-        {
-            var item = await _contexto.Livros.FindAsync(id.ToString());
-
-            if (item == null)
+            if (livroBanco == null)
                 return NotFound();
 
-            return item;
+            return Ok(livroBanco);
+        }
+
+        [HttpGet("Listar/")]
+        public async Task<ActionResult<IEnumerable<Livro>>> ObterLivros()
+        {
+            var livroBanco = await _context.Livros.ToListAsync();
+
+            return Ok(livroBanco);
+        }
+
+        [HttpPut("Atualizar/{id}")]
+        public async Task<ActionResult<Livro>> AtualizarLivro(int id, Livro livro)
+        {
+            var livroBanco = await _context.Livros.FindAsync(id);
+
+            if (livroBanco == null)
+                return NotFound();
+
+            livroBanco.Nome = livro.Nome;
+            livroBanco.Autor = livro.Autor;
+            livroBanco.Categoria = livro.Categoria;
+            livroBanco.Preco = livro.Preco;
+            livroBanco.Quantidade = livro.Quantidade;
+            livroBanco.Imagem = livro.Imagem;
+
+            _context.Livros.Update(livroBanco);
+            await _context.SaveChangesAsync();
+
+            return Ok(livroBanco);
+        }
+
+        [HttpDelete("Apagar/{id}")]
+        public async Task<ActionResult<Livro>> DeletarLivro(int id)
+        {
+            var livroBanco = await _context.Livros.FindAsync(id);
+
+            if (livroBanco == null)
+                return NotFound();
+
+            _context.Livros.Remove(livroBanco);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
         }
     }
 }
