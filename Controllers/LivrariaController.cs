e3d20ae6a25fa591f9d1536a81d8a90e27efc3d2
MarcosGerminiodeSouza <marcos.germiniodesouza@gmail.com>
2023-12-18 22:35:10 -0300

    LivrariaAPI Controller CRUD Http

--- Controllers/LivrariaController.cs
+++ Controllers/LivrariaController.cs
@@ -18,6 +18,24 @@ namespace LivrariaAPI.Controllers
         public LivrariaController(LivrariaDbContext context)
         {
             _context = context;
+
+            //rollback banco de dados (mocking)
+            foreach (Livro x in _context.Livros)
+                _context.Livros.Remove(x);
+            _context.SaveChanges();
+
+            _context.Livros.Add(new Livro { Id = 1, Nome = "SHERLOCK HOLMES", Autor = "Arthur Conan Doyle", Categoria = "Mistério", Preco = 89.94M, Quantidade = 5, Imagem = "img1" });
+            _context.Livros.Add(new Livro { Id = 2, Nome = "O MUNDO DE SOFIA", Autor = "Jostein Gaarder", Categoria = "Românce", Preco = 19.94M, Quantidade = 6, Imagem = "img2" });
+            _context.Livros.Add(new Livro { Id = 3, Nome = "ARCÈNE LUPIN O Ladrão de Casaca", Autor = "Maurice Leblanc", Categoria = "Ficção", Preco = 29.90M, Quantidade = 10, Imagem = "img3" });
+            _context.Livros.Add(new Livro { Id = 4, Nome = "INTRODUÇÃO À PROGRAMAÇÃO", Autor = "Anita Lopes", Categoria = "Programação", Preco = 120.00M, Quantidade = 9, Imagem = "img4" });
+            _context.Livros.Add(new Livro { Id = 5, Nome = "Guia Front-End", Autor = "Diego ES", Categoria = "Programação", Preco = 115.15M, Quantidade = 8, Imagem = "img5" });
+            _context.Livros.Add(new Livro { Id = 6, Nome = "Aprenda a Programar com C#", Autor = "António Trigo", Categoria = "Programação", Preco = 99.99M, Quantidade = 10, Imagem = "img6" });
+            _context.Livros.Add(new Livro { Id = 7, Nome = "Use a Cabeça! C#", Autor = "Andrew Stellman", Categoria = "Programação", Preco = 100.94M, Quantidade = 5, Imagem = "img7" });
+            _context.Livros.Add(new Livro { Id = 8, Nome = "Introdução à linguagem SQL", Autor = "Tomas Nield", Categoria = "Banco de Dados", Preco = 149.90M, Quantidade = 10, Imagem = "img8" });
+            _context.Livros.Add(new Livro { Id = 9, Nome = "MySQL Aprendendo na Prática", Autor = "Sérgio Luiz Tonsig", Categoria = "Banco de Dados", Preco = 188.80M, Quantidade = 7, Imagem = "img9" });
+            _context.Livros.Add(new Livro { Id = 10, Nome = "O PODER DO HÁBITO", Autor = "Charles Duhigg", Categoria = "Autoajuda", Preco = 50.00M, Quantidade = 6, Imagem = "img10" });
+
+            _context.SaveChanges();
         }
 
         [HttpPost]
