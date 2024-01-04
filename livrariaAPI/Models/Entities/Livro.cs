d9cba1ed1f02e37720f8f82074140e323a0f28d6
MarcosGerminiodeSouza <marcos.germiniodesouza@gmail.com>
2024-01-04 14:15:46 -0300

    Nova Modelagem

--- livrariaAPI/Models/Entities/Livro.cs
+++ livrariaAPI/Models/Entities/Livro.cs
@@ -9,5 +9,18 @@ namespace livrariaAPI.Models.Entities
     public class Livro
     {
         public int LivroId { get; set; }
+        public Autor Autor { get; set; }
+        public int AutorId { get; set; }
+        public Categoria Categoria { get; set; }
+        public int CategoriaId { get; set; }
+        public Editora Editora { get; set; }
+        public int EditoraId { get; set; }
+        public string Titulo { get; set; }
+        public string Temporada { get; set; }
+        public int NumeroDoAno { get; set; }
+        public decimal ValorLivro { get; set; }
+        public string Img { get; set; }
+        public string Lancamento { get; set; }
+        public int QuantidadeLivroEstoque { get; set; }
     }
 }
